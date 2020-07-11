492d5f117abe608ce38440beadf6ccd693a8c124
Brett Karr <b.karr@polyclip.us>
2020-07-11 13:08:39 -0500

    up to 14 done

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,6 +12,10 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
+print("x: %d, y: %2.2f, z: %s" % (x,y,z));
+
 # Use the 'format' string method to print the same thing
+print("x: {0:5d}, y: {1:8.2f}, z: {2:s}".format(x,y,z))
 
 # Finally, print the same thing using an f-string
+print(F"x: {x}, y: {y}, z: {z}")
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    data = f.read()
+    print(data)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +21,15 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    f.write('something random 1\n')
+    f.write('something else random 2\n')
+    f.write('something more random 3')
+
+print('file closed: ', f.closed, '\n')
+
+with open('bar.txt', 'r') as f:
+    data = f.read()
+    print(data)
+
+print('file closed: ', f.closed)
